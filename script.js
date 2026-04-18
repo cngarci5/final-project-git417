5d319000b8045ea1132ddab7a08afa70ae326b54
cngarci5 <cngarci5@asu.edu>
2026-04-17 18:52:15 -0700

    product display

--- script.js
+++ script.js
@@ -15,7 +15,28 @@ toggleButton.addEventListener("click", () => {
 });
 
 // Product Display
+const selectors = document.querySelectorAll(".selector");
+const platformLogo = document.getElementById("platform-logo");
 
+const platformImages = {
+    ea : "images/ea-svgrepo-com.svg",
+    playstation: "images/playstation-svgrepo-com.svg",
+    xbox: "images/xbox-svgrepo-com.svg",
+    steam: "images/steam-svgrepo-com.svg"
+
+};
+
+selectors.forEach(button => {
+    button.addEventListener("click", () => {
+
+        selectors.forEach(button => button.classList.remove("active"));
+
+        button.classList.add("active");
+
+        const platform = button.dataset.platform;
+        platformLogo.src = platformImages[platform];
+});
+});
 
 // Game
 
